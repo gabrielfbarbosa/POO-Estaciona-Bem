bdbbf9fbed93da27244d2cec84472b5a92d0dc31
gabrielfbarbosa <gfelipebarbosa@yahoo.com>
2024-04-14 14:20:29 -0300

    Inicando projeto

---
+++ App/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/*
+ * @author Gabriel Felipe Barbosa
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Salvee!");
+    }
+}
